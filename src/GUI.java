8efa0753aba31b110c51bff0f30a57472fbd0472
Dell <nourraasshhraf@gmail.com>
2022-12-31 19:02:47 +0200

    Final

--- src/GUI.java
+++ src/GUI.java
@@ -1,5 +1,5 @@
 import net.sf.clipsrules.jni.CLIPSException;
-import net.sf.clipsrules.jni.CLIPSLoadException;
+
 import net.sf.clipsrules.jni.Environment;
 import javax.swing.*;
 import java.awt.event.*;
@@ -9,17 +9,20 @@ import java.util.ArrayList;
 public class GUI extends JFrame implements ActionListener{
     public static Environment clips = new Environment();
 
+
     JLabel l;
     
     ArrayList<JRadioButton>cb=new ArrayList<>(5);
     JButton b;
     QueryGUI q;
-   // public   Problem2GUI p2 ;
+
     GUI(){
+        setTitle("University Expert System");
+
         l=new JLabel("Choose the query");
         l.setBounds(50,50,300,20);
-        cb.add(new JRadioButton("Query 1 "));
-        cb.get(0).setBounds(100,100,150,20);
+        cb.add(new JRadioButton("Get all TAs belonging to a department "));
+        cb.get(0).setBounds(100,100,400,20);
         cb.get(0).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -29,8 +32,8 @@ public class GUI extends JFrame implements ActionListener{
 
             }
         });
-        cb.add(new JRadioButton("Query 2 "));
-        cb.get(1).setBounds(100,150,150,20);
+        cb.add(new JRadioButton("Get the courses a student is currently studying"));
+        cb.get(1).setBounds(100,150,400,20);
         cb.get(1).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -39,8 +42,8 @@ public class GUI extends JFrame implements ActionListener{
                 }
             }
         });
-        cb.add(new JRadioButton("Query 3 "));
-        cb.get(2).setBounds(100,200,150,20);
+        cb.add(new JRadioButton("Get all the courses offered by a certain faculty "));
+        cb.get(2).setBounds(100,200,400,20);
         cb.get(2).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -49,8 +52,8 @@ public class GUI extends JFrame implements ActionListener{
                 }
             }
         });
-        cb.add(new JRadioButton("Query 4 "));
-        cb.get(3).setBounds(100,250,150,20);
+        cb.add(new JRadioButton("Get Lecturers of a faculty"));
+        cb.get(3).setBounds(100,250,400,20);
         cb.get(3).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -59,8 +62,8 @@ public class GUI extends JFrame implements ActionListener{
                 }
             }
         });
-        cb.add(new JRadioButton("Query 5 "));
-        cb.get(4).setBounds(100,300,150,20);
+        cb.add(new JRadioButton("Get all faculties of a university"));
+        cb.get(4).setBounds(100,300,400,20);
         cb.get(4).addActionListener(new ActionListener() {
             @Override
             public void actionPerformed(ActionEvent e) {
@@ -78,7 +81,7 @@ public class GUI extends JFrame implements ActionListener{
         b.setBounds(100,350,80,30);
         b.addActionListener(this);
         add(l);add(b);
-        setSize(400,450);
+        setSize(450,450);
         setLayout(null);
         setVisible(true);
         setDefaultCloseOperation(EXIT_ON_CLOSE);
@@ -96,26 +99,16 @@ public class GUI extends JFrame implements ActionListener{
                     throw new RuntimeException(ex);
                 }
             clips.clear();
+            q.clearFile("out.dat");
         }
-        q.clearFile("out.dat");
     }
     public void actionPerformed(ActionEvent e){
         try {
             clipsRun();
-
-            if (cb.get(0).isSelected()) {
-
-            } else if (cb.get(1).isSelected()) {
-                clipsRun();
-            } else if (cb.get(2).isSelected()) {
-                clipsRun();
-            } else if (cb.get(3).isSelected()) {
-                clipsRun();
-            } else if (cb.get(4).isSelected()) {
-                clipsRun();
-            }
         } catch(CLIPSException ex){
-            ex.printStackTrace();
+            JOptionPane.showMessageDialog(this,"NOT FOUND IN THIS EXPERT SYSTEM");
+
+            //ex.printStackTrace();
         }
 
     }
